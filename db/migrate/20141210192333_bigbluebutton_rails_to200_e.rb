6ed82d1aeae86a029eae38a39181657e34ed9fff
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-04-12 21:12:50 -0300

    Fix error from 3d9c13515bbb88a1ccee146dea49fb30bf571fec

--- db/migrate/20141210192333_bigbluebutton_rails_to200_e.rb
+++ db/migrate/20141210192333_bigbluebutton_rails_to200_e.rb
@@ -1,6 +1,6 @@
 class BigbluebuttonRailsTo200E < ActiveRecord::Migration
   def up
-    remove_index :bigbluebutton_rooms, :voice_bridge if index_exists?(:books, :created_at)
+    remove_index :bigbluebutton_rooms, :voice_bridge if index_exists?(:bigbluebutton_rooms, :voice_bridge)
   end
 
   def down
