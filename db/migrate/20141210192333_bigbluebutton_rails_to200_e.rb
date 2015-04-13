3d9c13515bbb88a1ccee146dea49fb30bf571fec
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-04-12 21:09:20 -0300

    Prevent migration BigbluebuttonRailsTo200E from breaking
    When the index doesn't exist.

--- db/migrate/20141210192333_bigbluebutton_rails_to200_e.rb
+++ db/migrate/20141210192333_bigbluebutton_rails_to200_e.rb
@@ -1,6 +1,6 @@
 class BigbluebuttonRailsTo200E < ActiveRecord::Migration
   def up
-    remove_index :bigbluebutton_rooms, :voice_bridge
+    remove_index :bigbluebutton_rooms, :voice_bridge if index_exists?(:books, :created_at)
   end
 
   def down
